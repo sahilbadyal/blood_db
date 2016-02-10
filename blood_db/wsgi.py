f67fb6251ef8a7765199d73c6882f69f99e65f4a
sahilbadyal <sbadyals@gmail.com>
2016-02-10 06:38:53 +0530

    added to heroku

--- blood_db/wsgi.py
+++ blood_db/wsgi.py
@@ -13,6 +13,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blood_db.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application =Cling(get_wsgi_application())
 
 #application = get_wsgi_application()
